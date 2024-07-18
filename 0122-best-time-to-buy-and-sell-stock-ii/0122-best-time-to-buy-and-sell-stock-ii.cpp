0d07fe660f3dd405da67b316d71a63e473d446bc
Sarthak Nirgude <100946019+Sarthak000001@users.noreply.github.com>
2024-07-18 11:15:18 +0530

    Time: 12 ms (15.23%), Space: 20.6 MB (6.18%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    // buy -> 1 & buy -> 0 (sell)
+    int solve(int ind,int buy,vector<int>&prices,vector<vector<int>>&dp){
+        if(ind >= prices.size()){
+            return 0;
+        }
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        long long maxProfit = 0;
+        if(buy){
+            maxProfit = max(-prices[ind] + solve(ind+1,0,prices,dp),
+                           0 + solve(ind+1,1,prices,dp));
+        }
+        else{
+            maxProfit = max(prices[ind] + solve(ind+1,1,prices,dp),
+                           0 + solve(ind+1,0,prices,dp));
+        }
+        return dp[ind][buy] = maxProfit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return solve(0,1,prices,dp);
+    }
+};
