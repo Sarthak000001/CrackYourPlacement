595f162a684e9f1288b1cedab8b0ba7e7382bcd9
Sarthak Nirgude <100946019+Sarthak000001@users.noreply.github.com>
2024-07-19 17:43:08 +0530

    Time: 49 ms (15.81%), Space: 44.9 MB (15.64%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -5,7 +5,7 @@ public:
         // TC - O(2 * k), SC - O(1)
         int leftSum = 0,rightSum = 0,maxSum = 0;
         for(int i=0;i < k;i++) leftSum += cardPoints[i];
-        maxSum = leftSum;
+        maxSum = leftSum; //imp step
         if(n == k) return maxSum;
         int rightIndex = n-1;
         for(int i=k-1;i >= 0;i--){ 
